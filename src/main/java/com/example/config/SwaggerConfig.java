2c300232bfbd6a8a9da98ce6223676b7e22b5376
1194314874@qq.com <zeng123456>
2025-08-15 16:27:14 +0800

    添加文件

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -3,12 +3,18 @@ package com.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +26,8 @@ public class SwaggerConfig {
                 .groupName("系统API接口")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
-                .build().apiInfo(apiInfo());
+                .build().apiInfo(apiInfo())
+                .globalOperationParameters(getGlobalParameters());// 添加全局参数;
     }
 
     @Bean
@@ -29,7 +36,8 @@ public class SwaggerConfig {
                 .groupName("业务API接口")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.web"))
-                .build().apiInfo(apiInfo());
+                .build().apiInfo(apiInfo())
+                .globalOperationParameters(getGlobalParameters());// 添加全局参数;
     }
 
     private ApiInfo apiInfo() {
@@ -41,4 +49,20 @@ public class SwaggerConfig {
                 .contact(new Contact("曾衍育", "http://example.com", "1194314874@qq.com"))
                 .build();
     }
+
+    /**
+     * 添加Swagger的全局参数
+     */
+    private List<Parameter> getGlobalParameters() {
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(new ParameterBuilder()
+                .name("Authorization") // 请求头的名称
+                .description("Token") // 描述信息
+                .modelRef(new ModelRef("string")) // 类型，这里是string类型
+                .parameterType("header") // 参数类型，这里是header类型
+                .required(true) // 是否必填，这里设置为false，表示非必填，可以根据实际需求设置
+                .build());
+        return parameters;
+    }
+
 }
