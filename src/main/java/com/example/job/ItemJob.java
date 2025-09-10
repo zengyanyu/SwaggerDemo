58d5ff91da68a542171911849c10cba587b15ad4
1194314874@qq.com <zeng123456>
2025-09-10 17:12:42 +0800

    添加文件

--- src/main/java/com/example/job/ItemJob.java
+++ src/main/java/com/example/job/ItemJob.java
@@ -1,19 +1,17 @@
 package com.example.job;
 
+import com.example.util.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Slf4j
 public class ItemJob implements Job {
 
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss EEEE");
-
     @Override
     public void execute(JobExecutionContext context) {
-        log.info("currentTime ==>> " + DATE_FORMAT.format(new Date()));
+        log.info("currentTime ==>> " + DateUtils.SIMPLE_DATE_FORMAT.format(new Date()));
     }
 }
